df1fdc3e0902797b45e51840749c622499f6494d
A-Emilia <CHKW@live.dk>
2026-05-23 20:17:00 +0200

    I think this is correct usage for retrieving the strings.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -62,7 +62,7 @@ builder.Services.AddCors(options =>
     });
 });
 
-string mongoConnectionString = builder.Configuration.GetConnectionString("MONGO_AZURE_CONNECTIONSTRING")
+string mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_AZURE_CONNECTIONSTRING")
     ?? throw new InvalidOperationException("Missing Azure Cosmos connection string.");
 
 builder.Services.AddSingleton<IMongoClient>(_ =>
@@ -80,7 +80,7 @@ builder.Services.AddScoped<IMeasurementRepository, MeasurementRepository>();
  * I need to dependency inject these.
  */
 
-string postgresConnectionString = builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING")
+string postgresConnectionString = Environment.GetEnvironmentVariable("AZURE_POSTGRESQL_CONNECTIONSTRING")
         ?? throw new InvalidOperationException("Missing Postgres connection string.");
 
 builder.Services.AddScoped<IUserRepository>(_ => new UserRepository(postgresConnectionString));
